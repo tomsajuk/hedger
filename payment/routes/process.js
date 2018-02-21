f286f939dd2d379c65e2006555b7f0a62d498e95
tomsajuk <tomsajuk@gmail.com>
2018-02-21 18:27:57 +0530

    Signed-off-by: tomsajuk <tomsajuk@gmail.com>

---
+++ payment/routes/process.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+
+router.post('/',function(req, res, next) {
+	console.log(req.body);
+	var payment_id = req.body.razorpay_payment_id;
+	var url = 'https://'+KEY_ID+':'+SECRET_KEY'@api.razorpay.com/v1/payments/';
+	console.log(url+payment_id);
+	request(url+payment_id, function (error, response, body) {
+	  console.log('Response:', body);
+	  var payment = JSON.parse(body);
+	  console.log(payment.amount/100);
+	});
+	res.send('Cool');
+});
+
+router.get('/check',function(req,res,next) {
+	console.log("working");
+	res.send('sucess');
+});
+
+module.exports = router;
