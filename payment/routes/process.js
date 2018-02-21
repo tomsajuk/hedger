44d486184e394fdfb9be384ad9f638618b5edeef
tomsajuk <tomsajuk@gmail.com>
2018-02-21 19:08:46 +0530

    node.js
    Signed-off-by: tomsajuk <tomsajuk@gmail.com>

--- payment/routes/process.js
+++ payment/routes/process.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var KEY_ID = process.env.KEY_ID;
+var SECRET_KEY = process.env.SECRET_KEY;
+
 
 router.post('/',function(req, res, next) {
 	console.log(req.body);
 	var payment_id = req.body.razorpay_payment_id;
-	var url = 'https://'+KEY_ID+':'+SECRET_KEY'@api.razorpay.com/v1/payments/';
+	var url = 'https://'+KEY_ID+':'+SECRET_KEY+'@api.razorpay.com/v1/payments/';
 	console.log(url+payment_id);
 	request(url+payment_id, function (error, response, body) {
 	  console.log('Response:', body);
