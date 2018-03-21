365bbb13d0c4cf9f3d02c3426b09ad21aa3c1169
tomsajuk <tomsajuk@gmail.com>
2018-03-21 16:01:26 +0530

    firebas

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,8 +1,12 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+//var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.EMAIL_ID,
+    privateKey: '-----BEGIN PRIVATE KEY-----\n'+process.env.FIREBASE_KEY+'\n-----END PRIVATE KEY-----\n'
+  }),
   databaseURL: "https://hedger-invest.firebaseio.com",
   databaseAuthVariableOverride: {
     uid: "1234567890"
